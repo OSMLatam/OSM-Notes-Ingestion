5ee23c82221c622e95aca154f54848f9e4df01e0
Andres Gomez Casanova <angoca@yahoo.com>
2022-12-10 19:37:00 -0500

    Script to prepare the database for WMS

---
+++ NotesWMS/prepareDatabase.sql
@@ -0,0 +1,62 @@
+-- Script to synchronize notes data with another table used for WMS.
+--
+-- Author: Andres Gomez (AngocA)
+-- Version: 2022-12-10
+
+-- Creates another table with only the necessary columns for WMS.
+CREATE TABLE IF NOT EXISTS notes_wms AS
+ SELECT
+  note_id,
+  extract(year from created_at) AS year_created_at,
+  extract (year from closed_at) AS year_closed_at,
+  ST_SetSRID(ST_MakePoint(longitude, latitude), 4326) AS geometry
+ FROM notes
+;
+
+-- Function for trigger when inserting new notes.
+CREATE OR REPLACE FUNCTION insert_new_notes()
+  RETURNS TRIGGER AS
+ $$
+ BEGIN
+  INSERT INTO notes_wms
+   VALUES
+   (
+    NEW.note_id,
+    extract(year from NEW.created_at),
+    extract(year from NEW.closed_at),
+    ST_SetSRID(ST_MakePoint(NEW.longitude, NEW.latitude), 4326)
+   )
+  ;
+ END;
+ $$ LANGUAGE plpgsql
+;
+
+-- Function for trigger when updating notes. This applies for 2 cases:
+-- * From open to close (solving).
+-- * From close to open (reopening).
+-- It is not used when adding a comment.
+CREATE OR REPLACE FUNCTION update_notes()
+  RETURNS TRIGGER AS
+ $$
+ BEGIN
+  UPDATE notes_wms
+   SET closed_at = NEW.closed_at
+  ;
+ END;
+ $$ LANGUAGE plpgsql
+;
+
+-- Trigger for new notes.
+CREATE OR REPLACE TRIGGER insert_new_notes
+  AFTER INSERT ON notes
+  FOR EACH ROW
+  EXECUTE FUNCTION insert_new_notes()
+;
+
+-- Trigger for updated notes.
+CREATE OR REPLACE TRIGGER update_notes
+  AFTER UPDATE ON notes
+  FOR EACH ROW
+  WHEN (OLD.closed_at IS DISTINCT FROM NEW.closed_at)
+  EXECUTE FUNCTION update_notes()
+;
