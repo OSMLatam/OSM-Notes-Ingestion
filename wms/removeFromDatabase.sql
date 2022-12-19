75e3037b2130f3cf42bf2a6d37de4e0554d50fa2
Andres Gomez Casanova <angoca@yahoo.com>
2022-12-18 21:58:20 -0500

    semi colon

--- wms/removeFromDatabase.sql
+++ wms/removeFromDatabase.sql
@@ -4,5 +4,5 @@ DROP FUNCTION wms.update_notes();
 DROP FUNCTION wms.insert_new_notes();
 DROP INDEX notes_closed;
 DROP INDEX notes_open;
-DROP TABLE wms.notes_wms
+DROP TABLE wms.notes_wms;
 DROP SCHEMA wms;
