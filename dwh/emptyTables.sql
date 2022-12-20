80bd66f8ac57bb06478dcba7ae11360eb5e8ab01
Andres Gomez <angoca@yahoo.com>
2022-12-20 00:05:22 -0500

    Shows time

--- dwh/emptyTables.sql
+++ dwh/emptyTables.sql
@@ -1,4 +1,7 @@
+SELECT CURRENT_TIMESTAMP AS Processing, 'Deleting facts';
 DELETE FROM dwh.facts;
+SELECT CURRENT_TIMESTAMP AS Processing, 'Deleting users';
 DELETE FROM dwh.dimension_users;
+SELECT CURRENT_TIMESTAMP AS Processing, 'Deleting countries';
 DELETE FROM dwh.dimension_countries;
 
