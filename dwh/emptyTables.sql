002db6a7720c716cb53ac01b28cab0cd7a9b6a4f
Andres Gomez <angoca@yahoo.com>
2022-12-20 23:25:38 -0500

    Deletes time dimension

--- dwh/emptyTables.sql
+++ dwh/emptyTables.sql
@@ -1,5 +1,7 @@
 SELECT CURRENT_TIMESTAMP AS Processing, 'Deleting facts';
 DELETE FROM dwh.facts;
+SELECT CURRENT_TIMESTAMP AS Processing, 'Deleting time';
+DELETE FROM dwh.dimension_time;
 SELECT CURRENT_TIMESTAMP AS Processing, 'Deleting users';
 DELETE FROM dwh.dimension_users;
 SELECT CURRENT_TIMESTAMP AS Processing, 'Deleting countries';
