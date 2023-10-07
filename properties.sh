ae6c1aaba548352f41577b640ece66934faaf6bf
Andres Gomez <angoca@yahoo.com>
2023-10-07 04:23:32 +0000

    Global properties

---
+++ properties.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set of properties for all scripts.
+#
+# Author: Andres Gomez
+# Version: 2023-10-06
+
+# Name of the Postgresql database to connect.
+declare -r DBNAME=notes
+
+# Mails to send the report.
+declare -r EMAILS="${EMAILS:-angoca@yahoo.com}"
+
+
