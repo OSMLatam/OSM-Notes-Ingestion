f7f6d9bdd5ab04da34c84f8d766723d12acdd999
Andres Gomez <angoca@yahoo.com>
2023-11-06 20:54:34 +0000

    Process notes when empty

--- bin/process/processAPINotes.sh
+++ bin/process/processAPINotes.sh
@@ -286,6 +286,7 @@ function __createApiTables {
 # Creates table properties during the execution.
 function __createPropertiesTable {
  __log_start
+ set -e
  __logi "Creating properties table"
  psql -d "${DBNAME}" -v ON_ERROR_STOP=1 -f "${POSTGRES_CREATE_PROPERTIES_TABLE}"
  __log_finish
@@ -462,9 +463,12 @@ function main() {
  __checkBaseTables || RET=${?}
  set -e
  if [[ "${RET}" -ne 0 ]] ; then
-  __logw "Creating base tables. It will take several hours."
+  __logw "Creating base tables. It will take half an hour."
   "${NOTES_SYNC_SCRIPT}" --base
   __logw "Base tables created."
+  __logi "This could take several minutes."
+  "${NOTES_SYNC_SCRIPT}"
+  __logw "Finished full synchronization from Planet."
  fi
  
  set -E
