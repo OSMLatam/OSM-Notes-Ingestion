e92daf61fefda72d6de989f9e88255d2f270c3e9
Andres Gomez <angoca@yahoo.com>
2023-11-06 16:13:29 +0000

    Show number of facts

--- sql/dwh/Staging-loadNotes.sql
+++ sql/dwh/Staging-loadNotes.sql
@@ -3,8 +3,13 @@
 -- Author: Andres Gomez (AngocA)
 -- Version: 2023-10-31
 
+SELECT COUNT(1) FROM dwh.facts;
+
 SELECT CURRENT_TIMESTAMP AS Processing, 'Inserting facts' AS Task;
 
 CALL staging.process_notes_actions_into_dwh();
 
 SELECT CURRENT_TIMESTAMP AS Processing, 'Facts inserted' AS Task;
+
+SELECT COUNT(1) FROM dwh.facts;
+
