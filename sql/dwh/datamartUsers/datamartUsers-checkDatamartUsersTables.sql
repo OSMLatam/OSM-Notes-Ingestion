6b2e7386b5dfa07db5ca84f25cb1754d12808f75
Andres Gomez <angoca@yahoo.com>
2023-11-10 19:42:47 +0000

    Typo

--- sql/dwh/datamartUsers/datamartUsers-checkDatamartUsersTables.sql
+++ sql/dwh/datamartUsers/datamartUsers-checkDatamartUsersTables.sql
@@ -12,7 +12,7 @@
    FROM INFORMATION_SCHEMA.TABLES
    WHERE TABLE_TYPE LIKE 'BASE TABLE'
    AND TABLE_SCHEMA LIKE 'dwh'
-   AND TABLE_NAME = 'datamartUsers'
+   AND TABLE_NAME = 'datamartusers'
    ;
    IF (qty <> 1) THEN
     RAISE EXCEPTION 'Base tables are missing: datamartUsers';
