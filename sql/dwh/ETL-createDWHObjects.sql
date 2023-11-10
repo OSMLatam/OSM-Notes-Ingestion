28fde0da3c5d086071dfafe17dcde1bf1969f846
Andres Gomez Casanova <angoca@yahoo.com>
2023-11-10 12:00:22 -0500

    Columns reordered, modified used, ToDos

--- sql/dwh/ETL-createDWHObjects.sql
+++ sql/dwh/ETL-createDWHObjects.sql
@@ -8,12 +8,12 @@ CREATE SCHEMA IF NOT EXISTS dwh;
 CREATE TABLE IF NOT EXISTS dwh.facts (
  fact_id SERIAL,
  id_note INTEGER NOT NULL,
- dimension_id_country INTEGER,
- processing_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
- action_at TIMESTAMP,
+ dimension_id_country INTEGER NOT NULL,
+ processing_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+ action_at TIMESTAMP NOT NULL,
  action_comment note_event_enum NOT NULL,
- action_dimension_id_date INTEGER,
- action_dimension_id_hour INTEGER,
+ action_dimension_id_date INTEGER NOT NULL,
+ action_dimension_id_hour INTEGER NOT NULL,
  action_dimension_id_user INTEGER,
  opened_dimension_id_date INTEGER NOT NULL,
  opened_dimension_id_hour INTEGER NOT NULL,
@@ -26,7 +26,8 @@ CREATE TABLE IF NOT EXISTS dwh.facts (
 CREATE TABLE IF NOT EXISTS dwh.dimension_users (
  dimension_user_id SERIAL,
  user_id INTEGER NOT NULL,
- username VARCHAR(256)
+ username VARCHAR(256),
+ modified BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS dwh.dimension_countries (
@@ -41,12 +42,12 @@ CREATE TABLE IF NOT EXISTS dwh.dimension_countries (
 CREATE TABLE IF NOT EXISTS dwh.dimension_days (
  dimension_day_id SERIAL,
  date_id DATE,
- days_from_notes_epoch INTEGER,
- days_to_next_year INTEGER
+ days_from_notes_epoch INTEGER, -- TODO who uses
+ days_to_next_year INTEGER -- TODO who uses
 );
 
 CREATE TABLE IF NOT EXISTS dwh.dimension_times (
  dimension_time_id SERIAL,
  hour SMALLINT,
- morning BOOLEAN -- true for am, false for pm.
+ morning BOOLEAN -- true for am, false for pm. TODO Who uses
 );
