912180e0a5fac76de7111aa18194f1637e535f00
Andres Gomez <angoca@yahoo.com>
2023-11-10 19:44:09 +0000

    Variable corrected

--- sql/dwh/datamartUsers/datamartUsers-alterTableAddYears.sql
+++ sql/dwh/datamartUsers/datamartUsers-alterTableAddYears.sql
@@ -4,9 +4,9 @@
 -- Version: 2023-11-09
   
 ALTER TABLE dwh.datamartUsers
- ADD COLUMN history_$YEAR_open INTEGER,
- ADD COLUMN history_$YEAR_commented INTEGER,
- ADD COLUMN history_$YEAR_closed INTEGER,
- ADD COLUMN history_$YEAR_closed_with_comment INTEGER,
- ADD COLUMN history_$YEAR_reopened INTEGER
+ ADD COLUMN history_${YEAR}_open INTEGER,
+ ADD COLUMN history_${YEAR}_commented INTEGER,
+ ADD COLUMN history_${YEAR}_closed INTEGER,
+ ADD COLUMN history_${YEAR}_closed_with_comment INTEGER,
+ ADD COLUMN history_${YEAR}_reopened INTEGER
 ;
