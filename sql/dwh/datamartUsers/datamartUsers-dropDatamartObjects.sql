ba8762e871c87645ce3482d0fd0f3fe24311831d
Andres Gomez <angoca@yahoo.com>
2023-11-10 19:43:50 +0000

    Order and all objects

---
+++ sql/dwh/datamartUsers/datamartUsers-dropDatamartObjects.sql
@@ -0,0 +1,16 @@
+-- Drop datamart for users tables.
+--
+-- Author: Andres Gomez (AngocA)
+-- Version: 2023-10-29
+
+DROP PROCEDURE dwh.update_datamart_user_activity_year;
+
+DROP PROCEDURE dwh.insert_datamart_user;
+
+DROP TABLE dwh.badges_per_users;
+
+DROP TABLE dwh.badges;
+
+DROP TABLE dwh.datamartUsers;
+
+DROP TABLE dwh.contributor_types;
