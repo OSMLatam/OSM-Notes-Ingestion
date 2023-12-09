2acb8b32318f7aa2f3e356a2db86c2db6f0582ac
Andres Gomez Casanova <angoca@yahoo.com>
2023-12-09 00:15:00 -0500

    Time to sleep

--- etc/properties.sh
+++ etc/properties.sh
@@ -1,35 +1,48 @@
 #!/bin/bash
 
-# Set of properties for all scripts.
+# Set of properties for all scripts. This file should be modified for specific
+# customization.
 #
 # Author: Andres Gomez
-# Version: 2023-10-06
-
-# OpenStreetMap API URL
-# shellcheck disable=SC2034
-declare -r OSM_API="https://api.openstreetmap.org/api/0.6"
+# Version: 2023-12-09
 
 # Name of the Postgresql database to connect.
 # shellcheck disable=SC2034
-declare -r DBNAME=notes
+declare -r DBNAME="notes"
 
-# User to connect to the database
+# User to connect to the database.
 # shellcheck disable=SC2034
-declare -r DB_USER=notes
+declare -r DB_USER="notes"
 
-# Mails to send the report to.
-declare -r EMAILS="${EMAILS:-angoca@yahoo.com}"
 
-# OpenStreetMap Planet dump
+# Mails to send the report about checking the differences between planet and database.
+declare -r EMAILS="username@domain.com"
+
+
+# OpenStreetMap API URL.
+# shellcheck disable=SC2034
+declare -r OSM_API="https://api.openstreetmap.org/api/0.6"
+
+# OpenStreetMap Planet dump.
 # shellcheck disable=SC2034
 declare -r PLANET="https://planet.openstreetmap.org"
 
-# Overpass interpreter.
+
+# Overpass interpreter. Used to download the contries and maritimes boundaries.
 # shellcheck disable=SC2034
 declare -r OVERPASS_INTERPRETER="https://overpass-api.de/api/interpreter"
 
+# Wait between loops when downloading boundaries, to prevent "Too many
+# requests".
+# shellcheck disable=SC2034
+declare -r SECONDS_TO_WAIT="3"
+
+
 # Get location in processPlanet.
 # Quantity of notes to process per loop, to get the location of the note.
-declare -r LOOP_SIZE="${LOOP_SIZE:-10000}"
+# shellcheck disable=SC2034
+declare -r LOOP_SIZE="10000"
+
 # Number of parallel threads to process notes to get the location.
-declare -r PARALLELISM="${PARALLELISM:-5}"
+# shellcheck disable=SC2034
+declare -r PARALLELISM="5"
