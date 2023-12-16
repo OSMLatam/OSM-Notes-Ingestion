2100a6e605d1944b970bfb2550a3c29c93a15f7d
Andres Gomez <angoca@yahoo.com>
2023-12-16 16:15:28 +0000

    Drop second SP

--- sql/dwh/Staging-initialFactsLoadDrop.sql
+++ sql/dwh/Staging-initialFactsLoadDrop.sql
@@ -1,7 +1,9 @@
 -- Loads data warehouse data for year ${YEAR}.
 --
 -- Author: Andres Gomez (AngocA)
--- Version: 2023-12-09
+-- Version: 2023-12-16
+
+DROP PROCEDURE staging.process_notes_actions_into_dwh_${YEAR};
 
 DROP PROCEDURE staging.process_notes_at_date_${YEAR};
 
