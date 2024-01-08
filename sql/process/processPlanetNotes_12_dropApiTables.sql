8f0dbdb88fec8eba7e54f95abed942bd49ebe714
Andres Gomez <angoca@yahoo.com>
2024-01-08 17:20:51 +0000

    File renamed

--- sql/process/processPlanetNotes_12_dropApiTables.sql
+++ sql/process/processPlanetNotes_12_dropApiTables.sql
@@ -4,5 +4,6 @@
 -- Version: 2023-10-25
   
 DROP TABLE IF EXISTS max_note_timestamp;
+DROP TABLE IF EXISTS note_comments_text_api;
 DROP TABLE IF EXISTS note_comments_api;
 DROP TABLE IF EXISTS notes_api;
