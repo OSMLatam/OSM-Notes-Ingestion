d5ef47b0a732bb64271cd2382b67a17ed6336364
Andres Gomez <angoca@yahoo.com>
2024-01-08 17:27:40 +0000

    Renamed, trigget to put sequence

--- sql/process/processPlanetNotes_41_loadSyncNotes.sql
+++ sql/process/processPlanetNotes_41_loadSyncNotes.sql
@@ -31,3 +31,43 @@ SELECT /* Notes-processPlanet */ CURRENT_TIMESTAMP AS Processing,
 SELECT /* Notes-processPlanet */ CURRENT_TIMESTAMP AS Processing,
  COUNT(1) AS Qty, 'Uploaded sync comments' AS Text
 FROM note_comments_sync;
+
+DO /* Notes-processPlanet-assignSequence-sync */
+$$
+DECLARE
+  m_current_note_id INTEGER;
+  m_previous_note_id INTEGER;
+  m_sequence_value INTEGER;
+  m_rec_note_comment_sync RECORD;
+  m_note_comments_sync_cursor CURSOR  FOR
+   SELECT
+    note_id
+   FROM note_comments_sync
+   ORDER BY note_id, id
+   FOR UPDATE;
+
+ BEGIN
+  OPEN m_note_comments_sync_cursor;
+
+  LOOP
+   FETCH m_note_comments_sync_cursor INTO m_rec_note_comment_sync;
+   -- Exit when no more rows to fetch.
+   EXIT WHEN NOT FOUND;
+
+   m_current_note_id := m_rec_note_comment_sync.note_id;
+   IF (m_previous_note_id = m_current_note_id) THEN
+    m_sequence_value := m_sequence_value + 1;
+   ELSE
+    m_sequence_value := 1;
+    m_previous_note_id := m_current_note_id;
+   END IF;
+
+   UPDATE note_comments_sync
+    SET sequence_action = m_sequence_value
+    WHERE CURRENT OF m_note_comments_sync_cursor;
+  END LOOP;
+
+  CLOSE m_note_comments_sync_cursor;
+
+END
+$$;
