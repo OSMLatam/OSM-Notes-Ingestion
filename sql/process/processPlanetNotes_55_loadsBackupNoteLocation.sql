7175e201039079880e6f4268fdf9511b777804df
Andres Gomez <angoca@yahoo.com>
2024-01-19 04:40:52 +0000

    Loads already calulated location notes

---
+++ sql/process/processPlanetNotes_55_loadsBackupNoteLocation.sql
@@ -0,0 +1,27 @@
+-- Loads the old notes locations into the databse, and then updates the
+-- note's location.
+--
+-- Author: Andres Gomez (AngocA)
+-- Version: 2024-01-18
+
+DROP TABLE IF EXISTS backup_note_locations;
+CREATE TABLE backup_note_locations (
+  note_id INTEGER,
+  id_country INTEGER
+);
+
+SELECT /* Notes-processAPI */ CURRENT_TIMESTAMP AS Processing,
+ 'Loading old note location...' AS Text;
+COPY backup_note_locations (note_id, id_country)
+FROM '${CSV_BACKUP_NOTE_LOCATION}' csv;
+SELECT /* Notes-processAPI */ CURRENT_TIMESTAMP AS Processing,
+ 'Locations loaded. Updating notes...' AS Text;
+UPDATE notes AS n
+ SET id_country = b.id_country
+ FROM backup_note_locations AS b
+ WHERE b.note_id = n.note_id
+ AND n.id_country IS NULL;
+SELECT /* Notes-processAPI */ CURRENT_TIMESTAMP AS Processing,
+ 'Notes updated with location...' AS Text;
+
+DROP TABLE backup_note_locations;
