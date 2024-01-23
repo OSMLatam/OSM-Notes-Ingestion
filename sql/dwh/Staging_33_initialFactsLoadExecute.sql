d5a354d92a6d2ef4598ec7dcaa2bde58a8d327ae
Andres Gomez <angoca@yahoo.com>
2024-01-23 00:46:13 +0000

    Messages around the call

--- sql/dwh/Staging_33_initialFactsLoadExecute.sql
+++ sql/dwh/Staging_33_initialFactsLoadExecute.sql
@@ -1,6 +1,12 @@
 -- Loads data warehouse data for year ${YEAR}.
 --
 -- Author: Andres Gomez (AngocA)
--- Version: 2023-12-19
+-- Version: 2024-01-22
+
+SELECT /* Notes-staging */ CURRENT_TIMESTAMP AS Processing,
+ 'Processing year ${YEAR}' AS Text;
 
 CALL staging.process_notes_actions_into_staging_${YEAR}();
+
+SELECT /* Notes-staging */ CURRENT_TIMESTAMP AS Processing,
+ 'Year ${YEAR} processed' AS Text;
