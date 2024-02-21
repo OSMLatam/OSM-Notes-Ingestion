990be80826437a2b1536dee770a781808db6f5e0
Andres Gomez <angoca@yahoo.com>
2024-02-21 03:48:17 +0000

    Ignore last update value

--- sql/process/processAPINotes_32_insertNewNotesAndComments.sql
+++ sql/process/processAPINotes_32_insertNewNotesAndComments.sql
@@ -1,7 +1,7 @@
 -- Bulk notes and notes comments insertion.
 --
 -- Author: Andres Gomez (AngocA)
--- Version: 2024-02-19
+-- Version: 2024-02-20
 
 SELECT /* Notes-processAPI */ CURRENT_TIMESTAMP AS Processing,
   COUNT(1) Qty, 'current notes - before' AS Text
@@ -12,12 +12,8 @@ $$
  DECLARE
   r RECORD;
   m_closed_time VARCHAR(100);
-  m_lastupdate TIMESTAMP;
   m_stmt VARCHAR(200);
  BEGIN
-  SELECT /* Notes-processAPI */ timestamp
-   INTO m_lastupdate
-  FROM max_note_timestamp;
 
   FOR r IN
    SELECT /* Notes-processAPI */ note_id, latitude, longitude, created_at,
@@ -28,13 +24,12 @@ $$
    m_closed_time := QUOTE_NULLABLE(r.closed_at);
 
    INSERT INTO logs (message) VALUES (r.note_id || ' - created:'
-    || r.created_at || ',last:' || m_lastupdate || ',closed:'
-    || m_closed_time || '.');
+    || r.created_at || ',closed:' || m_closed_time || '.');
 
    m_stmt := 'CALL insert_note (' || r.note_id || ', ' || r.latitude || ', '
      || r.longitude || ', ' || 'TO_TIMESTAMP(''' || r.created_at
      || ''', ''YYYY-MM-DD HH24:MI:SS'')' || ', $PROCESS_ID' || ')';
-   --RAISE NOTICE 'Note % (%) %.', r.note_id, m_stmt, m_lastupdate;
+   --RAISE NOTICE 'Note % (%) %.', r.note_id, m_stmt;
    EXECUTE m_stmt;
    INSERT INTO logs (message) VALUES (r.note_id || ' - Note inserted.');
   END LOOP;
@@ -58,12 +53,8 @@ $$
  DECLARE
   r RECORD;
   m_created_time VARCHAR(100);
-  m_lastupdate TIMESTAMP;
-  m_stmt VARCHAR(200);
+  m_stmt VARCHAR(500);
  BEGIN
-  SELECT /* Notes-processAPI */ timestamp
-   INTO m_lastupdate
-  FROM max_note_timestamp;
 
   FOR r IN
    SELECT /* Notes-processAPI */ note_id, event, created_at, id_user,
@@ -71,14 +62,6 @@ $$
    FROM note_comments_api
    ORDER BY created_at, sequence_action
   LOOP
-   IF (r.created_at <= m_lastupdate) THEN
-    INSERT INTO logs (message) VALUES (r.note_id || ' - Comment - created:'
-     || r.created_at || ',last:' || m_lastupdate || ',event:' || r.event
-     || '.');
-    -- Rejects all comments before the latest processed.
-    INSERT INTO logs (message) VALUES (r.note_id || ' - Comment skipped.');
-    CONTINUE;
-   END IF;
 
    IF (r.id_user IS NOT NULL) THEN
     m_stmt := 'CALL insert_note_comment (' || r.note_id || ', '
@@ -95,7 +78,7 @@ $$
       || 'NULL, '
       || QUOTE_NULLABLE(r.username) || ', $PROCESS_ID' || ')';
    END IF;
-   RAISE NOTICE 'Comment % (%).', m_stmt, m_lastupdate;
+   --RAISE NOTICE 'Comment %.', m_stmt;
    EXECUTE m_stmt;
    INSERT INTO logs (message) VALUES (r.note_id
      || ' - Comment for note inserted.');
