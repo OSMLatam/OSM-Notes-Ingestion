a25aa43884f0925e36f794202116ae91c0c6e658
Andres Gomez <angoca@yahoo.com>
2025-07-01 21:54:26 +0000

    Tests for xslt transformations

---
+++ test/xslt/test_xslt.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Test the XSLT files to produce the expected output.
+#
+# Author: Andres Gomez (AngocA)
+# Version: 2025-07-01
+
+XSLT_DIR="../../xslt"
+
+function test() {
+ echo "Testing ${TEST} for ${TYPE}."
+ xsltproc "${XSLT_DIR}/${TEST}-${TYPE}-csv.xslt" "osm-notes-${TYPE}.xml" > "${TEST}-${TYPE}-actual.csv"
+ diff "${TEST}-${TYPE}-actual.csv" "${TEST}-${TYPE}-expected.csv"
+ if [ "${?}" -ne 0 ]; then
+  echo "ERROR: the generated file is different from the expected one - ${TEST}."
+ else
+  echo "Test passed - ${TEST}."
+ fi
+ echo
+}
+
+TEST="note_comments_text"
+TYPE="Planet"
+test
+TYPE="API"
+test
+echo
+
+TEST="note_comments"
+TYPE="Planet"
+test
+TYPE="API"
+test
+echo
+
+TEST="notes"
+TYPE="Planet"
+test
+TYPE="API"
+test
+
