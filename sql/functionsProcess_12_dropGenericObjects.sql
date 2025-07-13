f53d4f0d54eeb5ca3625924459e9d4b0205c70e7
Andres Gomez <angoca@yahoo.com>
2025-07-13 19:56:39 +0000

    New script to drop other objetcs

---
+++ sql/functionsProcess_12_dropGenericObjects.sql
@@ -0,0 +1,11 @@
+-- Drop generic objects.
+--
+-- Author: Andres Gomez (AngocA)
+-- Version: 2025-07-13
+
+DROP PROCEDURE IF EXISTS insert_note_comment;
+
+DROP PROCEDURE IF EXISTS insert_note;
+
+DROP FUNCTION IF EXISTS get_country;
+
